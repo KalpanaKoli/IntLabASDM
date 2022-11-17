8d4020ed83d5a4c42e71d091f2f20f24839574bd
KalpanaKoli <41873042+KalpanaKoli@users.noreply.github.com>
2022-11-17 21:29:09 +0530

    Add files via upload

---
+++ VMS.java
@@ -0,0 +1,12 @@
+package Inheritance;
+//Q wap for vehicle management system .define class vehicle ,car ,bike and VMS class
+
+public class VMS {
+
+	public static void main(String[] args)
+	{
+		 
+
+	}
+
+}
